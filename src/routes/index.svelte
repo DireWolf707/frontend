5ff0fb7e7231d5f3c56d6ebf53e8b15b70e2ae45
DireWolf707 <rahulsood707@gmail.com>
2022-03-29 00:07:27 +0530

    added home page (hero component)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,12 @@
-<h1>Hello World</h1>
+<div class="mx-6 my-12">
+    <div class="hero h-[60vh] bg-base-200 rounded-xl">
+        <div class="hero-content text-center">
+            <div class="max-w-md">
+                <h1 class="text-5xl font-bold">Hello there</h1>
+                <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                <a href="/user/login" class="btn btn-primary">Login</a>
+                <a href="/user/signup" class="btn btn-ghost btn-outline">Signup</a>
+            </div>
+        </div>
+    </div>
+</div>
