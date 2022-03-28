251ae49d332f6265166ebfb08ef37d668f8b86db
DireWolf707 <rahulsood707@gmail.com>
2022-03-28 17:11:05 +0530

    added header

---
+++ src/lib/UI/Header.svelte
@@ -0,0 +1,24 @@
+<div class="navbar bg-neutral text-neutral-content">
+
+    <div class="flex-1">
+        <button class="btn btn-ghost normal-case text-xl">Home</button>
+    </div>
+
+    <div class="flex-none">
+
+        <button class="btn btn-ghost rounded-btn">Button</button>
+        
+        <div class="dropdown dropdown-end">
+            <button class="btn btn-ghost rounded-btn">
+                Dropdown
+                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+            </button>
+            <ul class="dropdown-content menu p-2 shadow bg-neutral rounded-box w-52 mt-3">
+                <li><a>Item 1</a></li> 
+                <li><a>Item 2</a></li>
+            </ul>
+        </div>
+
+    </div>
+
+</div>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,7 @@
 <script>
     import "../app.css";
+    import Header from "$lib/UI/Header.svelte";
 </script>
 
+<Header />
 <slot />
