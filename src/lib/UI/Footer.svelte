0464f134fa3788fa755fb7ab23cd2569ea309b9a
DireWolf707 <rahulsood707@gmail.com>
2022-03-28 21:59:10 +0530

    added footer

---
+++ src/lib/UI/Footer.svelte
@@ -0,0 +1,14 @@
+<footer class="footer p-10 bg-neutral text-neutral-content">
+    <div>
+      <span class="footer-title">Services</span> 
+      <a href="/" class="link link-hover">Branding</a>
+    </div> 
+    <div>
+      <span class="footer-title">Company</span> 
+      <a href="/" class="link link-hover">About us</a>
+    </div> 
+    <div>
+      <span class="footer-title">Legal</span> 
+      <a href="/" class="link link-hover">Terms of use</a>
+    </div>
+  </footer>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,13 @@
 <script>
     import "../app.css";
     import Header from "$lib/UI/Header.svelte";
+    import Footer from "$lib/UI/Footer.svelte";
 </script>
 
-<Header />
-<slot />
+<div class="flex flex-col justify-center h-screen">
+    <Header />
+    <div class="mb-auto">
+        <slot />
+    </div>
+    <Footer />
+</div>
