462289faab3b8a54f7ca5572d71542de2b1dd7fe
DireWolf707 <rahulsood707@gmail.com>
2022-03-28 22:01:49 +0530

    fixed link tags from button to a

--- src/lib/UI/Header.svelte
+++ src/lib/UI/Header.svelte
@@ -1,12 +1,12 @@
 <div class="navbar bg-neutral text-neutral-content">
 
     <div class="flex-1">
-        <button class="btn btn-ghost normal-case text-xl">Home</button>
+        <a href="/" class="btn btn-ghost normal-case text-xl">Home</a>
     </div>
 
     <div class="flex-none">
 
-        <button class="btn btn-ghost rounded-btn">Button</button>
+        <a href="/" class="btn btn-ghost rounded-btn">Button</a>
         
         <div class="dropdown dropdown-end">
             <button class="btn btn-ghost rounded-btn">
@@ -14,8 +14,12 @@
                 <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
             </button>
             <ul class="dropdown-content menu p-2 shadow bg-neutral rounded-box w-52 mt-3">
-                <li><a>Item 1</a></li> 
-                <li><a>Item 2</a></li>
+                <li>
+                    <a href="/">Item 1</a>
+                </li> 
+                <li>
+                    <a href="/">Item 2</a>
+                </li>
             </ul>
         </div>
 
