227246ca6d40578d1624baf50dafba78d94b5854
DireWolf707 <rahulsood707@gmail.com>
2022-04-02 23:50:21 +0530

    added store init and loading bar in layout page

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,12 +2,33 @@
     import "../app.css";
     import Header from "$lib/UI/Header.svelte";
     import Footer from "$lib/UI/Footer.svelte";
+    import LoadingBar from "$lib/Reusable/LoadingBar.svelte";
+    import { userStore, getUser } from '$lib/Stores/user.js';
+    import { onMount } from "svelte";
+    
+    let loading = true;
+    onMount(async () => {
+        const [ data, err ] = await getUser();
+        // !err and data => login success
+        // err and !data => login fail
+        // err and data => server down
+        if (err && data) {
+            console.log(data);
+        } else {
+            userStore.set(data);
+            loading = false;
+        }
+    })
 </script>
 
 <div class="flex flex-col h-screen mx-2">
-    <Header />
-    <div class="mb-auto">
-        <slot />
-    </div>
-    <Footer />
+    {#if !loading}
+        <Header />
+        <div class="mb-auto">
+            <slot />
+        </div>
+        <Footer />
+    {:else}
+        <LoadingBar />
+    {/if}
 </div>
